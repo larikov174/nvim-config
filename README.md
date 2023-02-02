79390e8f56969278db365cb351a24b6e3923f448
Andrey Larikov <aalarikov@avito.ru>
2023-02-02 13:51:50 +0500

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## nvim-config
+
+---
+
+nvim and tmux settings
---
+++ nvim/lua/aalarikov/core/colorscheme.lua
@@ -0,0 +1,12 @@
+-- set colorscheme to nightfly with protected call
+-- in case it isn't installed
+local status, _ = pcall(vim.cmd, "colorscheme onedark")
+if not status then
+  print("Colorscheme not found!") -- print error if colorscheme not installed
+  return
+end
+
+require("onedark").setup({
+  style = "warm",
+})
+require("onedark").load()
