bc80666e9abad91bcacb2290ce7bec517489282f
Andrey Larikov <aalarikov@avito.ru>
2023-02-06 00:57:01 +0500

    add prettier default qoute settings

--- nvim/lua/aalarikov/plugins/lsp/null-ls.lua
+++ nvim/lua/aalarikov/plugins/lsp/null-ls.lua
@@ -17,7 +17,12 @@ null_ls.setup({
   sources = {
     --  to disable file types use
     --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-    formatting.prettier, -- js/ts formatter
+    formatting.prettier.with({
+      extra_args = {
+        "--single-quote",
+        "--jsx-signle-quote",
+      },
+    }), -- js/ts formatter
     formatting.stylua, -- lua formatter
     diagnostics.eslint_d.with({ -- js/ts linter
       -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
